6c711629c57518fa99a28f891297707804229b9a
christian cooney <christiancooney@hotmail.com>
2022-11-15 13:48:32 +0000

    functionality complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,46 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.all
+  end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params_create)
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params_update)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+
+  private
+
+  def task_params_create
+    params.require(:task).permit(:title, :details).with_defaults(completed: false)
+  end
+
+  def task_params_update
+    params.require(:task).permit(:title, :details, :completed)
   end
 
 end
